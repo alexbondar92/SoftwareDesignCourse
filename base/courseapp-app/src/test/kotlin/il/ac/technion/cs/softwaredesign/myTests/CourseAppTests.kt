72ab5acb74298b8834c5f4dbc74efd651cbd709b
alex bondar <alex.bondar@campus.technion.ac.il>
2019-04-28 12:42:40 +0300

    1st stage, Tests

--- base/courseapp-app/src/test/kotlin/il/ac/technion/cs/softwaredesign/myTests/CourseAppTests.kt
+++ base/courseapp-app/src/test/kotlin/il/ac/technion/cs/softwaredesign/myTests/CourseAppTests.kt
@@ -4,16 +4,44 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.present
 import il.ac.technion.cs.softwaredesign.CourseApp
 import il.ac.technion.cs.softwaredesign.CourseAppInitializer
+import jdk.nashorn.internal.objects.NativeArray.every
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.time.Duration
 
+import il.ac.technion.cs.softwaredesign.storage.read
+import il.ac.technion.cs.softwaredesign.storage.write
+import io.mockk.*
 
 class CourseAppStaffTest {
     private val courseAppInitializer = CourseAppInitializer()
 
+//    init {
+//        courseAppInitializer.setup()
+//    }
+
     init {
+        val storageMock= mutableMapOf<ByteArray,ByteArray>()
         courseAppInitializer.setup()
+        mockkStatic("il.ac.techinion.cs.softwaredesing.storage.secureStorageKt")
+        val keySlot = slot<ByteArray>()
+        val valueSlot = slot<ByteArray>()
+        every {
+            write(capture(keySlot), capture(valueSlot))
+        } answers {
+            val value = valueSlot.captured
+            val key = keySlot.captured
+            // println("now we are writing the value:$value with the key $key")
+            storageMock[key] = value
+        }
+        every {
+            read(capture(keySlot))
+        } answers {
+            val key = keySlot.captured
+            val value = storageMock[key]
+            Thread.sleep(value?.size?.toLong() ?: 0)
+            storageMock[key]
+        }
     }
 
     private val courseApp = CourseApp()
