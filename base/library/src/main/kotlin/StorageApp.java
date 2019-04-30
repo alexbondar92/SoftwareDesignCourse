844ec7037d6a364df1a67255b88eff1fa6218bb6
alex bondar <alex.bondar@campus.technion.ac.il>
2019-04-30 18:47:39 +0300

    Storage API as Library

---
+++ base/library/src/main/kotlin/StorageApp.java
@@ -0,0 +1,9 @@
+public class StorageApp {
+//    public fun read(key: String): String? {
+//        //implement
+//    }
+//
+//    public fun write(key: String, value: String): kotlin.Unit {
+//        //implement
+//    }
+}
