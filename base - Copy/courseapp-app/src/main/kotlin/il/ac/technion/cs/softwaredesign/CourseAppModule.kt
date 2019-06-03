3cde4c5812fd314e41110ccb6319b902f1117092
Alex Bondar <Alex.Bondar@campus.technion.ac.il>
2019-06-03 16:31:10 +0300

    clean branch

--- base - Copy/courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/CourseAppModule.kt
+++
@@ -1,9 +0,0 @@
-package il.ac.technion.cs.softwaredesign
-
-import com.authzee.kotlinguice4.KotlinModule
-
-class CourseAppModule : KotlinModule() {
-    override fun configure() {
-        TODO("Implement me!")
-    }
-}
--- base - Copy/courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/exceptions/NoSuchEntityException.kt
+++
@@ -1,3 +0,0 @@
-package il.ac.technion.cs.softwaredesign.exceptions
-
-class NoSuchEntityException(message: String? = null, cause: Throwable? = null) : Exception(message, cause)
